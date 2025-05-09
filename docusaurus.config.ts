b19753b4b40202fcf745d5295af38b4172d738d3
Moeenahmad01 <moeen.ahmad@logicielservice.com>
2025-05-09 22:31:28 +0500

    language

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -43,10 +43,10 @@ const config: Config = {
         label: 'English',
       },
       ja: {
-        label: 'Japanese',
+        label: '日本語',
       },
       th: {
-        label: 'Thai',
+        label: 'แบบไทย',
       },
     },
   },
