b47af08a9a70a667aa00779b97e4dd7402f744ac
Moeenahmad01 <moeen.ahmad@logicielservice.com>
2025-05-19 20:31:03 +0500

    swagger removed

--- src/pages/swagger.js
+++ src/pages/swagger.js
@@ -5,27 +5,29 @@ import SwaggerUI from 'swagger-ui-react'; // Swagger UI React component
 import 'swagger-ui-react/swagger-ui.css'; // Swagger UI styling
 import useIsBrowser from '@docusaurus/useIsBrowser';
 
-// export default function SwaggerPage() {
-//   return (
-//     <Layout
-//       title="Card API (Swagger)"
-//       description="Interact with the Card API using Swagger UI"
-//     >
-//       <div style={{ padding: '2rem', height: '100vh' }}>
-//         {/* <SwaggerUI url="/openapi.yml" /> */}
-//       </div>
-//     </Layout>
-//   );
-// }
-
 export default function SwaggerPage() {
   const isBrowser = useIsBrowser();
-  const SwaggerUI = isBrowser ? require('swagger-ui-react') : null;
-
+  // const SwaggerUI = isBrowser ? require('swagger-ui-react') : null;
   return (
-    <>
-      {isBrowser && SwaggerUI && <SwaggerUI url="/openapi.yml" />}
-    </>
+    <Layout
+      title="Card API (Swagger)"
+      description="Interact with the Card API using Swagger UI"
+    >
+      <div style={{ padding: '2rem', height: '100vh' }}>
+        {/* <SwaggerUI url="/openapi.yml" /> */}
+      </div>
+    </Layout>
   );
 }
+
+// export default function SwaggerPage() {
+//   const isBrowser = useIsBrowser();
+//   const SwaggerUI = isBrowser ? require('swagger-ui-react') : null;
+
+//   return (
+//     <>
+//       {isBrowser && SwaggerUI && <SwaggerUI url="/openapi.yml" />}
+//     </>
+//   );
+// }
 
