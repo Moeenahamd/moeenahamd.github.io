9fb9ac5e23fcdbdef2018db0b780b4d457b50809
moeenahamd <moeenranjha01@gmail.com>
2025-06-17 22:54:39 +0500

    add build

--- src/components/SwaggerUIComponent.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import SwaggerUI from 'swagger-ui-react';
-import 'swagger-ui-react/swagger-ui.css';
-
-const SwaggerUIComponent = () => {
-  return <SwaggerUI url="/openapi.yml" />;
-};
-
-export default SwaggerUIComponent;
